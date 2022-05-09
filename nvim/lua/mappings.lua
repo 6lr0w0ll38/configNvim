efb05522318f0051ee6c08f2ffe71ab424991dc6
6lr0w0ll38 <6lr0w0ll38@gmail.com>
2022-05-09 23:01:36 +0700

    Fix

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -90,8 +90,8 @@ cmp.setup {
         ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
+            elseif require'luasnip'.expand_or_jumpable() then
+                require'luasnip'.expand_or_jump()
             else
                 fallback()
             end
@@ -99,8 +99,8 @@ cmp.setup {
         ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
+            elseif require'luasnip'.jumpable(-1) then
+                require'luasnip'.jump(-1)
             else
                 fallback()
             end
